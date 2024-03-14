1a3e49c72cc319d1f1477c9823c88bac5b6ed0bf
codezilla322 <petermanning2177@gmail.com>
2024-03-13 23:27:44 -0500

    project done

---
+++ app.js
@@ -0,0 +1,91 @@
+require("dotenv").config();
+
+const express = require("express");
+const axios = require("axios");
+const app = express();
+const port = 8080;
+
+const FILLOUT_API_KEY = process.env.FILLOUT_API_KEY;
+
+app.get("/:formId/filteredResponses", async (req, res) => {
+  const formId = req.params.formId;
+  const limit = req.query.limit;
+  const afterDate = req.query.afterDate;
+  const beforeDate = req.query.beforeDate;
+  const offset = req.query.offset;
+  const status = req.query.status;
+  const includeEditLink = req.query.includeEditLink;
+  const sort = req.query.sort;
+  const filtersStr = req.query.filters;
+  let filters = JSON.parse(filtersStr ? filtersStr : "[]");
+  if (!Array.isArray(filters)) filters = [filters];
+
+  const response = await axios.get(
+    `https://api.fillout.com/v1/api/forms/${formId}/submissions?${
+      limit ? `limit=${limit}&` : ""
+    }${afterDate ? `afterDate=${afterDate}&` : ""}${
+      beforeDate ? `beforeDate=${beforeDate}&` : ""
+    }${offset ? `offset=${offset}&` : ""}${status ? `status=${status}&` : ""}${
+      includeEditLink ? `includeEditLink=${includeEditLink}&` : ""
+    }${sort ? `sort=${sort}&` : ""}`,
+    {
+      headers: {
+        Authorization: `Bearer ${FILLOUT_API_KEY}`,
+      },
+    }
+  );
+
+  const responses = response.data.responses;
+  const filteredResponses = responses.filter((response) => {
+    let questions = {};
+
+    response.questions.map((question) => {
+      questions[question.id] = question.value;
+    });
+
+    const filtered = filters.reduce((filtered, filter, index) => {
+      if (!filtered) return filtered;
+
+      const questionValue = questions[filter.id];
+      if (questionValue === undefined && filter.condition !== "does_not_equal")
+        return false;
+
+      switch (filter.condition) {
+        case "equals":
+          if (questionValue === filter.value) {
+            return true;
+          } else {
+            return false;
+          }
+        case "does_not_equal":
+          if (questionValue !== filter.value) {
+            return true;
+          } else {
+            return false;
+          }
+        case "greater_than":
+          if (questionValue > filter.value) {
+            return true;
+          } else {
+            return false;
+          }
+        case "less_than":
+          if (questionValue < filter.value) {
+            return true;
+          } else {
+            return false;
+          }
+        default:
+          return false;
+      }
+    }, true);
+
+    return filtered;
+  });
+
+  res.json({ ...response.data, responses: filteredResponses });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
