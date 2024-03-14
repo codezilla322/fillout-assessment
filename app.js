278c8e7df5a6ef088dc42796bbd386d22fdca7b9
codezilla322 <petermanning2177@gmail.com>
2024-03-13 23:42:47 -0500

    port change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const app = express();
-const port = 8080;
+const port = 80;
 
 const FILLOUT_API_KEY = process.env.FILLOUT_API_KEY;
 
